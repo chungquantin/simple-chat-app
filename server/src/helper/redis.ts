5fa7201ad42ddfbfc5c87bb86a98f1fbe6aad8dc
Chung Quan Tin <s3818487@rmit.edu.vn>
2021-01-31 23:56:21 +0700

    add redis config

--- server/src/helper/redis.ts
+++ server/src/helper/redis.ts
@@ -2,18 +2,18 @@ import * as Redis from "ioredis";
 import * as connectRedis from "connect-redis";
 import { REDIS_SESSION_PREFIX } from "../constants/global-variables";
 import { RedisPubSub } from "graphql-redis-subscriptions";
+import { EnvironmentType } from "../utils/environmentType";
+import "dotenv/config";
 
 export class REDIS {
+	isProduction = process.env.NODE_ENV?.trim() == EnvironmentType.PROD;
 	private readonly config: Redis.RedisOptions = {
-		port: 6379, // Redis port
-		host: "127.0.0.1", // Redis host
-		retryStrategy: (times) => {
-			// reconnect after
-			return Math.min(times * 50, 2000);
-		},
+		port: this.isProduction ? parseInt(process.env.REDIS_PORT as string) : 6379, // Redis port
+		host: this.isProduction ? (process.env.REDIS_HOST as string) : "127.0.0.1", // Redis host,
+		password: this.isProduction ? (process.env.REDIS_PASSWORD as string) : "",
 	};
 	public server = new Redis(this.config);
-	client = new Redis(this.config);
+	public client = new Redis(this.config);
 }
 
 export const redisPubSub = new RedisPubSub({
